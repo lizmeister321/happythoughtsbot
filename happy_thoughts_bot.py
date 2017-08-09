577d4b2cd196dfe09ba045c490d5d038ce6dda47
Lizzie Ellis <e.geiger.ellis@gmail.com>
2017-08-08 22:56:54 -0400

    prelim flickr api things

--- happy_thoughts_bot.py
+++ happy_thoughts_bot.py
@@ -1,6 +1,8 @@
 import tweepy
 import json
 import flickrapi
+import random
+import urllib
 
 
 #import keys
@@ -18,7 +20,6 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-pp = pprint.PrettyPrinter()
 
 breaking_tweets = api.user_timeline('cnnbrk')
 #for tweet in breaking_tweets:
@@ -28,6 +29,18 @@ breaking_tweets = api.user_timeline('cnnbrk')
 #if new tweet:
 
 	#find random baby animal on Flickr
+api_key = config['flickr_api']['key']
+api_secret = config['flickr_api']['secret']
+flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
+
+animals = config['animals']
+
+#gotta mix the cute animals up
+pick_one = random.randrange(0, len(animals))
+random_animal = "baby " + animals[pick_one]
+print random_animal
+search_results = flickr.photos.search(text=random_animal, license=(1,2,3,4,5,6,7), sort='relevance', safe_search=1, per_page=20 , page=1)
+url = 
 	#store pic
 	#attach to tweet
 
